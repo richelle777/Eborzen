b1d61a2b22deb39b0bb84d52c2e550aa4fff056f
richelle777 <richellemayap@gmail.com>
2022-12-01 19:03:28 +0100

    version avec ajout de la fonctionnalite de suppression d'une playlist

--- src/main/java/org/isj/ing4/isi/music/repository/PlaylistRepository.java
+++ src/main/java/org/isj/ing4/isi/music/repository/PlaylistRepository.java
@@ -5,6 +5,7 @@ import org.isj.ing4.isi.music.model.Titre;
 import org.isj.ing4.isi.music.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -14,4 +15,8 @@ import java.util.List;
 public interface PlaylistRepository extends JpaRepository<Playlist, Integer>, JpaSpecificationExecutor<Playlist> {
     @Query(value = "SELECT intitule_playlist,id_playlist,playlist.user_id FROM `playlist` INNER JOIN `user` ON playlist.user_id=user.user_id  where user.email = :email" , nativeQuery = true)
     List<Playlist> findPlaylistByUser(String  email);
+
+    @Modifying
+    @Query(value = "DELETE FROM `playlist` where playlist.id_playlist = :id" , nativeQuery = true)
+    void deletePlaylistById(int id);
 }
